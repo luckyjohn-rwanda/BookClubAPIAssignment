a0a70bae903c29bc519630d79c3c179835bb5278
luckyjohn-rwanda <j.mbikeshim@alustudent.com>
2021-03-17 00:31:42 +0200

    Final code version for our Book Club API.

--- routes/borrowRouter.js
+++ routes/borrowRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBorrows, viewborrows, viewAllborrows, updateborrows, viewmemberborrow} from "../controllers/borrowController.js";
+import { addBorrows, viewborrows, viewAllborrows, updateborrows} from "../controllers/borrowController.js";
 
 const borrowRouter = express.Router();
 
@@ -15,8 +15,6 @@ borrowRouter.get("/", viewAllborrows);
 // Update a record
 borrowRouter.put("/:id", updateborrows);
 
-// view member borrowings
-borrowRouter.get("/:id", viewmemberborrow);
 
 
 export default borrowRouter;
